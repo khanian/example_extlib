0610cb5f0a2ebea4aa5138befcb0ce9b3febd764
khanian <khanylee@gmail.com>
2023-11-11 09:29:31 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomeWidget(),
+  ));
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter에서 외부 라이브러리 사용하기'),
+      ),
+      body: const Body(),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RandomWords();
+  }
+}
+
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList
+        .map(
+          (word) => Text(
+            word.asCamelCase,
+            style: TextStyle(fontSize: 32),
+          ),
+        )
+        .toList();
+
+    return Column(
+      children: widgets,
+    );
+  }
+}
